39eb3b47a0628ca1d8e48b083f32a012d95be400
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-02-18 11:42:25 +0100

    connessione client-server

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,7 +1,26 @@
 package Client;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainClient {
     public static void main(String[] args) {
+        System.out.println("CLIENT: avvio del client");
+
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter pw= new PrintWriter(outputStream);
+            pw.print("Ciao Server");
+            pw.flush();
+            System.out.println("ClIENT: il cliente ha inviato un messaggio");
+        } catch (IOException e) {
+            System.out.println("Porta non disponibile");
+        }
+
+
+
 
     }
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,7 +1,24 @@
 package Server;
 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
     public static void main(String[] args) {
+        System.out.println("SERVER: Inizio esecuzione");
+
+
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("SERVER: in attesa di richieste dal cliente");
+            Socket clientSocket = server.accept();
+            System.out.println("Server connesso");
+        } catch (IOException e) {
+            System.out.println("SERVER: ");
+        }
+
 
     }
 }
